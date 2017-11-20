92d8568846cbee03cc2fbf387e240a83df91ccdf
Nicholas Clancy <nicholasclancy@pooledenergy.com>
2017-11-20 16:30:58 +1100

    - More debugging

--- src/ios/iOSWifiConnect.m
+++ src/ios/iOSWifiConnect.m
@@ -8,8 +8,8 @@
     CDVPluginResult *pluginResult = nil;
 	NSString* ssid = [command argumentAtIndex:0];
 	NSString* password = [command argumentAtIndex:1];
-	NSLog(@"%@", ssid);
-	NSLog(@"%@", password);
+	NSLog(@"Ssid for connect @", ssid);
+	NSLog(@"Password for connect@", password);
 
 	if (@available(iOS 11.0, *)) {
 	    if (ssid && [ssid length]) {

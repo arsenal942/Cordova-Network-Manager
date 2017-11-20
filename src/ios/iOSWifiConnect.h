3022754436428d83ada116ed99194da336547277
Nicholas Clancy <nicholasclancy@pooledenergy.com>
2017-11-20 13:40:00 +1100

    - Rename the plugin file - Update plugin.xml to match what is required

--- src/ios/iOSWifiConnect.h
+++ src/ios/iOSWifiConnect.h
@@ -1,7 +1,6 @@
 #import <Cordova/CDV.h>
 
-@interface iOSWifiConnect : CDVPlugin {
-}
+@interface iOSWifiConnect : CDVPlugin 
 
 // The hooks for our plugin commands
 - (void)connectNetwork:(CDVInvokedUrlCommand *)command;

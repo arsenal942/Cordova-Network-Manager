697272ac4c00e53243d2cd8daa18bb32560b8ec8
Nicholas Clancy <n.a.clancy@gmail.com>
2017-12-06 13:53:22 +1100

    - Let's try this. Enable before we disable

--- src/android/cordovanetworkmanager/cordovaNetworkManager.java
+++ src/android/cordovanetworkmanager/cordovaNetworkManager.java
@@ -282,6 +282,7 @@ public class cordovaNetworkManager extends CordovaPlugin {
             // We disable the network before connecting, because if this was the last connection before
             // a disconnect(), this will not reconnect.
 
+			wifiManager.enableNetwork(currentNetworkId, true);
 			wifiManager.disableNetwork(currentNetworkId);
 			wifiManager.enableNetwork(networkIdToConnect, true);
 
